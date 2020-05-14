f819dac1f9845b5837084a918981329b8c81f5b7
fedre30 <alfanofederica95@gmail.com>
2020-05-14 15:37:21 +0200

    Geocoding and map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,30 +1,46 @@
 import { Ionicons } from "@expo/vector-icons";
 import * as WebBrowser from "expo-web-browser";
 import * as React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import { RectButton, ScrollView } from "react-native-gesture-handler";
 import { Title } from "../components/atoms/StyledText";
+import MapView from "react-native-maps";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { getLocation } from "../utils/map";
+import { GOOGLE_API_KEY } from "../api-config";
 
 export default function MapScreen() {
+  const [location, setLocation] = React.useState(null);
+
+  React.useEffect(() => {
+    getLocation().then((data) => {
+      setLocation({
+        latitude: data.latitude,
+        longitude: data.longitude,
+        latitudeDelta: 0.003,
+        longitudeDelta: 0.003,
+      });
+    });
+  }, []);
+
   return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={styles.contentContainer}
-    >
-      <View>
-        <Title>Map Screen</Title>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require("../assets/images/robot-dev.png")
-                : require("../assets/images/robot-prod.png")
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
+    <View style={styles.container}>
+      <View style={styles.autocomplete}>
+        <GooglePlacesAutocomplete
+          placeholder="Search"
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+          }}
+          style={styles.inputStyle}
+          query={{
+            key: GOOGLE_API_KEY,
+            language: "en",
+          }}
+        />
       </View>
-    </ScrollView>
+      <MapView style={styles.mapStyle} initialRegion={location} />
+    </View>
   );
 }
 
@@ -33,20 +49,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fafafa",
   },
-  contentContainer: {
-    justifyContent: "center",
+  autocomplete: {
+    width: Dimensions.get("window").width,
+    height: 90,
     paddingTop: 30,
   },
-  welcomeContainer: {
-    alignItems: "center",
-    marginTop: 10,
-    marginBottom: 20,
+  inputStyle: {
+    backgroundColor: "transparent",
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: "contain",
-    marginTop: 3,
-    marginLeft: -10,
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height - 30,
   },
 });

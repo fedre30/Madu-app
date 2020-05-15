fb4ba9e1d31c06260b5042ceaa97c4fcc52bcdde
Nini3L <mdcdu917@gmail.com>
2020-05-15 14:24:07 +0200

    first commit

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -3,17 +3,42 @@ import * as WebBrowser from "expo-web-browser";
 import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { RectButton, ScrollView } from "react-native-gesture-handler";
-import { Title } from "../components/atoms/StyledText";
+import { Thumbnail } from "../components/molecules/ProfileThumbnail";
 import Colors from "../constants/Colors";
 
 export default function ProfileScreen() {
+  //style={{backgroundColor: Colors.orange}}
   return (
     <ScrollView
       style={styles.container}
       contentContainerStyle={styles.contentContainer}
     >
-      <View>
-        <Title>Profile</Title>
+      
+      <View style={{ flex: 1, backgroundColor: 'yellow' }}>
+        <View style={{ flex: 1, backgroundColor: 'red' }}>
+          <Text>Profile</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: 'green' }}>
+          <Text>Defis realisé</Text>
+        </View>
+        <View style={{ flex: 3, backgroundColor: 'blue' }}>
+          <Text>Options</Text>
+          <View style={styles.CagnotteContainer}>
+            <Thumbnail style={styles.thumbnailOption1} >Cagnotte</Thumbnail>
+          </View>
+          <View style={{backgroundColor: "#DCFFF7", width: 162, height: 162}}>
+            <Thumbnail >Recompenses</Thumbnail>
+          </View>
+          <View style={{backgroundColor: "##EEE9FF", width: 162, height: 162}}>
+            <Thumbnail style={styles.thumbnailOption1} >Defis</Thumbnail>
+          </View>
+          <View style={{backgroundColor: "#FFF1E9", width: 162, height: 162}}>
+            <Thumbnail >Nouvelle adresse</Thumbnail>
+          </View>
+          <View style={{backgroundColor: "#FFE9E9", width: 162, height: 162}}>
+            <Thumbnail >Parametre</Thumbnail>
+          </View>
+        </View>
       </View>
     </ScrollView>
   );
@@ -22,10 +47,21 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.white,
+    backgroundColor: Colors.pink,
   },
   contentContainer: {
     justifyContent: "center",
     paddingTop: 30,
+    flex:1,
   },
+  CagnotteContainer: {
+    //backgroundColor: "#EBF3FF", 
+    backgroundColor: Colors.tintColor, 
+    width: 162, 
+    height: 162
+  },
+  thumbnailOption1:{  
+    backgroundColor: Colors.pink,
+    borderRadius: 10,
+  }
 });

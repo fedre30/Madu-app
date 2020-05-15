e9e0330913007625081648fdc66c3d10e1f26907
quentinaime <quentin.aime.dev@gmail.com>
2020-05-15 15:38:39 +0200

    add in-view navigation

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -14,7 +14,7 @@ export default function BottomTabNavigator({ navigation, route }) {
   // Set the header title on the parent stack navigator depending on the
   // currently active tab. Learn more in the documentation:
   // https://reactnavigation.org/docs/en/screen-options-resolution.html
-  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+  navigation.setOptions({ headerTitle: getHeaderTitle(route), headerShown: false });
 
   return (
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
@@ -52,7 +52,7 @@ export default function BottomTabNavigator({ navigation, route }) {
         name="Cagnotte"
         component={PointsScreen}
         options={{
-          title: "Cagnotte",
+          title: "Cagnotte test test",
           tabBarIcon: ({ focused }) => (
             <TabBarIcon focused={focused} name="md-gift" />
           ),

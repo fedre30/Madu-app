852ddcd9f58aae649c08e1eff8734f38cdc3dc89
joel POKAM <joel.pokam@hetic.net>
2020-05-31 13:16:31 +0200

    add header

--- screens/PointsScreen.js
+++ screens/PointsScreen.js
@@ -1,9 +1,10 @@
 import { Ionicons } from "@expo/vector-icons";
 import * as WebBrowser from "expo-web-browser";
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, Strong } from "react-native";
 import { RectButton, ScrollView } from "react-native-gesture-handler";
 import { Title } from "../components/atoms/StyledText";
+import { Row } from "native-base";
 
 export default function PointsScreen() {
   return (
@@ -13,6 +14,20 @@ export default function PointsScreen() {
     >
       <View>
         <Title>Cagnotte</Title>
+      </View>
+      <View style={styles.header}>
+        <Text style={styles.number}>150</Text>
+        <Image
+          source={require("../assets/images/Vector.png")}
+          style={styles.iconImage}
+        />
+        <Text style={styles.leave}>LEAVES</Text>
+      </View>
+      <View>
+        <Text style={styles.description}>
+          200 leafs à accumuler avant de pouvoir débloquer la prochaine
+          récompense.
+        </Text>
       </View>
     </ScrollView>
   );
@@ -25,6 +40,34 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     justifyContent: "center",
-    paddingTop: 30,
+    paddingTop: 40,
+  },
+  header: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 30,
+  },
+  number: {
+    fontWeight: "500",
+    fontSize: 48,
+    color: "#69FFD4",
+  },
+  iconImage: {
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  leave: {
+    fontSize: 25,
+    fontWeight: "500",
+    marginLeft: 20,
+    marginTop: 10,
+    textTransform: "uppercase",
+  },
+  description: {
+    marginTop: 18,
+    fontSize: 20,
+    fontWeight: "500",
+    marginLeft: 30,
   },
 });

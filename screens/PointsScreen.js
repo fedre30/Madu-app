28bd96a088ef4fb3a8566b35ebaecf21b8f79aa7
M3L-DC <mdcdu917@gmail.com>
2020-07-05 22:39:10 +0200

    page challenge finis avec données brute

--- screens/PointsScreen.js
+++ screens/PointsScreen.js
@@ -2,7 +2,7 @@ import { Ionicons } from "@expo/vector-icons";
 import * as React from "react";
 import { StyleSheet, Text, View, Image, FlatList, ScrollView } from "react-native";
 import Sliding from "../components/organisms/Sliding";
-import { RectButton, ScrollView } from "react-native-gesture-handler";
+import { RectButton } from "react-native-gesture-handler";
 import { Title } from "../components/atoms/StyledText";
 import { Row, Item } from "native-base";
 import Items from "../components/organisms/ListRecompense";
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 
 import { createStackNavigator } from '@react-navigation/stack';
@@ -7,7 +7,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Colors from "../constants/Colors";
 import Sliding from "../components/organisms/Sliding";
 import { Thumbnail } from "../components/molecules/ProfileThumbnail";
-//import { Challenges } from "./profile-subscreens/ChallengesScreen";
+import { ContentChallenges } from "./profile-subscreens/ContentChallengesScreen";
 import { NewAddress } from "./profile-subscreens/NewAddressScreen";
 // import {jackpotAndRewardsPageName} from "./profile-subscreens/jackpotAndRewardsPageName";
 
@@ -43,7 +43,7 @@ export const Profile = ({navigation}) => {
             height={129}
             left={96}
             top={10}
-            // onPress={() => navigation.navigate('Challenges')}
+            onPress={() => navigation.navigate('ContentChallenges')}
           >Mes défis
           </Thumbnail>
               
@@ -91,7 +91,7 @@ export default function ProfileScreen() {
   return (
     <PointsStack.Navigator>
       <PointsStack.Screen name="Profile" component={Profile} />
-      {/* <PointsStack.Screen name="ChallengesName" component={Challenges} /> */}
+      <PointsStack.Screen name="ContentChallenges" component={ContentChallenges} />
       {/* <PointsStack.Screen name="jackpotAndRewardsName" component={jackpotAndRewardsPageName} /> */}
       <PointsStack.Screen name="NewAddress" component={NewAddress} />
       <PointsStack.Screen name="Ranking" component={Ranking} />
